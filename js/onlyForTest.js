e435d0a5d211fe46f8ef12bfc1728765e6fc466a
Nicolas Sandvik Lound <nlound@gmail.com>
2013-07-25 21:38:11 -0300

    Collapsable columns commit
    And some dummy data to test.

--- js/onlyForTest.js
+++ js/onlyForTest.js
@@ -8,3 +8,13 @@ var datosTotales = [
 			];
 
 var col = ["#76a4c1","#cd8581","#d3c56b","#bbc5a0","#9988b3","#6f6f6f"]; //alpha .4 for the bars 
+
+
+var otrosDatos = [
+				{"n:": "Partido Uno", "v": "11231923" , "p": "30.3%"},
+				{"n": "Partido Dos", "v": "2231923" , "p": "20.1%"},
+				{"n": "Partido Tres", "v": "331923" , "p": "10.2%"},
+				{"n": "Partido cinco", "v": "41953" , "p": "4.5%"},
+				{"n": "Partido seis", "v": "31450" , "p": "3.2%"},
+				{"n": "Partido siete", "v": "21243" , "p": "2.1%"}
+			]
