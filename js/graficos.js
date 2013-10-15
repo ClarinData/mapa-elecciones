1d4a8e33904a4aaca9387a0568992a1c7d95da09
loudsandwich <nlound@gmail.com>
2013-10-15 18:37:36 -0300

    Funciones de dibujo de barras
    Falta debuguear.

--- js/graficos.js
+++ js/graficos.js
@@ -11,7 +11,8 @@ document.getElementById("camaBtn").addEventListener("click", dibuja, false);
 	
 function dibuja(){
 
-	var datos,
+	var queDatoMuestro,
+		datos,
 		v_txt = "Sin datos",
 		m_txt = "Sin datos";
 
@@ -96,10 +97,144 @@ function dibuja(){
 			v_txt = datos.votacion.votos.porcentaje.toLocaleString() + "%";
 	}
 
-	document.getElementById("graficoBarras").innerHTML = queDatoMuestro;
 
-
+	dibujaBarras(datos);
+//	document.getElementById("graficoBarras").innerHTML = queDatoMuestro;
 	
 	document.getElementById("mesas").innerHTML = m_txt;
 	document.getElementById("votos").innerHTML = v_txt;
+}
+
+
+function dibujaBarras(dataset){
+		
+	if (dataset){ // si hay datos 
+		if (dataset.id === "TOTALES"){ // barras totales pais
+			
+			d3.select("#graficoBarras").selectAll("div").remove();
+			var maximo = 200;
+			var dominio = d3.scale.linear().domain([0, dataset.votacion.partidos_politicos[0].votos]).range([0, maximo]);
+		
+			var contenido = d3.select("#graficoBarras").selectAll(".contenedorBar").data(function() {
+				console.log("dataset:" ,dataset);
+				return dataset.votacion.partidos_politicos;
+			});
+		
+			var contenidoEnter = contenido.enter().append("div").classed("contenedorBar", true);
+		
+			contenidoEnter.append("div").classed("nombreBar", true).text(function(d) {
+				return d.nombre;
+			});
+		
+			contenidoEnter.append("div").attr("class", function(d) {
+				var tempClass = "";
+				if (d.fuerza_politica) {
+					tempClass = "fp_" + d.fuerza_politica;
+				} else {
+					tempClass = "fp_SFP";
+				}
+				return "barraBar " + tempClass;
+			}).style("width", function(d) {
+				return Math.ceil(dominio(d.votos)) + "px";
+			});
+
+			contenidoEnter.append("div").classed("porcentajeBar", true).style("left", function(d) {
+				return Math.ceil(dominio(d.votos) + 5) + "px";
+			}).text(function(d) {
+				return d.porcentaje.toLocaleString() + "%";
+			});
+
+			contenidoEnter.append("div").classed("cantidadBar", true).style("left", function(d) {
+				return Math.ceil(dominio(d.votos) + 5) + "px";
+			}).text(function(d) {
+				return "(" + miles(d.votos.toString()) + ")";
+			});
+			
+			contenido.exit().remove();
+
+		}else{ // barras detalle provincia o departamento
+
+			var maximo = 120;
+			
+			d3.select("#graficoBarras").selectAll("div").remove();
+			d3.select("#referenciaBarras").style("background-position", "0 -115px");
+		
+			var dominio = d3.scale.linear().domain([0, dataset.votacion.partidos_politicos[0].votos]).range([0, maximo]);
+		
+			var contenido = d3.selectAll("#graficoBarras").selectAll(".contenedorBar").data(function(d) {
+				return dataset.votacion.partidos_politicos;
+			});
+		
+			var contenidoEnter = contenido.enter().append("div").classed("contenedorDet", true);
+		
+			contenidoEnter.append("div").classed("nombreDet", true).html(function(d) {
+				if (d.nombre !== undefined) {
+					return d.nombre;
+				} else {
+					return d.id.toUpperCase();
+				}
+		
+			});
+		
+		
+			contenidoEnter.append("span").classed("candidatoDet", true).text(function(d) {
+				if (d.id === "blancos"){return "";};
+				if (d.id === "nulos"){return "";};
+				if (d.id === "recurridos"){return "";};
+				return "N. Apellido / ";
+			});		
+
+			contenidoEnter.append("span").classed("cantidadDet", true).text(function(d) {
+				return miles(d.votos.toString()) + " votos";
+			});		
+		
+		
+			contenidoEnter.append("div").attr("class", function(d) {
+				var tempClass = "";
+				if (d.fuerza_politica !== undefined) {
+					tempClass = "fp_" + d.fuerza_politica;
+				} else {
+					tempClass = "fp_SFP";
+				}
+				return "barraDet " + tempClass;
+			}).style("width", function(d) {
+				return Math.ceil(dominio(d.votos)) + "px";
+			});
+		
+			contenidoEnter.append("div").classed("porcentajeDet", true).style("left", function(d) {
+					return Math.ceil(dominio(d.votos) + 5) + "px";
+				}).text(function(d) {
+					return d.porcentaje.toLocaleString() + "%";
+				});
+		
+			contenidoEnter.append("div").classed("lineaDet", true);
+
+			contenidoEnter.append("div").text(function(d) {
+					if (d.id === "blancos"){return "";};
+					if (d.id === "nulos"){return "";};
+					if (d.id === "recurridos"){return "";};
+					return "-";
+				}).attr("class", function(d) {
+				var tempClass = "";
+				if (d.fuerza_politica !== undefined) {
+					tempClass = "fp_" + d.fuerza_politica;
+				} else {
+					tempClass = "fp_SFP";
+					if (d.id === "blancos"){ tempClass = "";};
+					if (d.id === "nulos"){ tempClass = "";};
+					if (d.id === "recurridos"){tempClass = "";};
+				}
+				return "bancasDet " + tempClass;
+			});
+		
+			contenido.exit().remove();
+		}
+	
+	} else {
+		
+		d3.select("#graficoBarras").selectAll("div").remove();
+		d3.select("#graficoBarras").append("div").classed("sinDatos", true).text("Sin Datos Para Visualizar");
+		
+	}
+	
 }
